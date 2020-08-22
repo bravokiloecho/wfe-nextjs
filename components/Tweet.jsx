1bf52cbfaacc9936dd21006d433d7f223c29ee7d
Benjamin E <ben@elwyn.co>
2020-08-22 16:22:36 +0200

    resize font

--- components/Tweet.jsx
+++ components/Tweet.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import clamp from 'lodash/clamp'
 import { useHotkeys } from 'react-hotkeys-hook'
+
+import useOnResize from '@/hooks/useOnResize'
 
 import styles from '@/styles/Tweet.module.css'
 
 const Tweet = ({ tweet, totalTweets, setActiveTweetIndex }) => {
+  // CHANGE SLIDES
   const changeSlide = React.useCallback((direction = 'next') => {
     setActiveTweetIndex((activeTweetIndex) => {
       const mod = direction === 'previous' ? -1 : 1
@@ -17,6 +21,18 @@ const Tweet = ({ tweet, totalTweets, setActiveTweetIndex }) => {
   // KEYBOARD SHORTCUTS
   useHotkeys('left', () => changeSlide('previous'))
   useHotkeys('right', () => changeSlide('next'))
+  // RESIZE FONT
+  const { width: windowWidth, height: windowHeight } = useOnResize()
+  const fontSize = React.useMemo(() => {
+    const totalCharacters = tweet.text.length
+    const area = windowWidth * windowHeight
+    const characterRatio = 90 / 80
+    const size = area / (characterRatio * Math.log(totalCharacters) * 2000 * 1.6)
+    const sizeMax = 80
+    const sizeMin = 22
+    return `${clamp(size, sizeMin, sizeMax)}px`
+  }, [windowWidth, windowHeight, tweet.text])
+
   return (
     <a
       className={[styles.tweetContainer].join(' ')}
@@ -26,7 +42,12 @@ const Tweet = ({ tweet, totalTweets, setActiveTweetIndex }) => {
       role="button"
       aria-label="Next tweet"
     >
-      <p className={[styles.tweet].join(' ')}>{tweet.text}</p>
+      <p
+        className={[styles.tweet].join(' ')}
+        style={{ fontSize }}
+      >
+        {tweet.text}
+      </p>
     </a>
   )
 }
